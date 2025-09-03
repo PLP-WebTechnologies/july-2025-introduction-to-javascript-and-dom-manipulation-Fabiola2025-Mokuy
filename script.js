8abbf3b577fd08503eecc8bd1e322dd262cbecf4
Fabiola2025-Mokuy <mokuyfabiola@gmail.com>
2025-09-03 14:55:12 +0200

    up date Assigment w5

--- script.js
+++ script.js
@@ -4,73 +4,45 @@ let car ={
     year: 2018
 };
 console.log(car)
+
 let fruit = ['banana','citron','grape']
-console.log(fruit)
+console.log(fruit);
 
-console.log(3-2)
-console.log(20/2)
+let name = Fabiola;
+let gender = F;
+if (gender = M){
+    console.log(" you are Boy");
+    }
+    else{
+        console.log(" you are girl");
+    }
 
-//Comparison operators
-console.log(5=="5")//true
-console.log(3==="3")//false
-console.log(9 !="9")//false
 
-/* fontion declaration*/
-function greet {
-    ('Javascript is amazing');}
-    greet();
-   /*fonction expression*/
-   let amazing()
-{console.log("I so happy")}
-amazing()
-
-/* arrow function
-const variableName =() =>{statements}*/
-const salut = () =>{
-    alert("Come back");
+function gender(Female)
+{if (Female)
+console.log ("good girl")
 }
-salut();
-//Parameters and return values
-// parameters - input
-//return- output
-function greetUser (name){
-    alert("greetings, "+name +". Bien Venue" );
+else{
+    console.log("good boy");
 }
-greetUser("Manuel Gib");
-greetUser("Marcos Baie");
 
-const square = function (num){
-    return num * num;
-};
-console.log(square(100));
-const PI = 3.14152;
-let circunference = (radius) =>{
-return 2 * PI * radius;}
-console.log(circunference(10));
-
-/* DOM - Document object Model*/
-function maniputate(){
-
-let body =document.getElementBYId('body');
-let intro =document.getElementBYId('intro');
-let ptext =document.getElementBYId('ptext');
-let bnt =document.getElementBYId('bnt');
+function maritalStatus (ms)
+{if (ms=s)
+    {console.log ("single");}
+else if (s=m){console.log("married");}
+else{ console.loge ("widower")}
 }
-// begin manipulation
+
+
+for (let i=1;i<=10;i++);{
+    console.log("Well come-",i);
+}
+
+document.getElementsByClassName("name").addEventlistener("click", function(){document.getElementsByClassName("name").innerTex =" write name";});
+//begin manipulation
 body.style.background = "pink";
-intro.textContent = " Heading has been altered";
-intro.style.color = "blue"
-ptext.style.fontSise ="20px"
-btn.style.boderColor ="marrow"
-btn.style.boder = "35px"
-
-
-
-
-
-
-
-
-
-
-
+classNamelassName.textContent = " Heading has been altered";
+className.style.color = "blue";
+ptext.style.fontSise ="20px";
+btn.style.boderColor ="marrow";
+btn.style.boder = "40px";

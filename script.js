d02d704d3cb5b20ca3904a544c14d47a1f523eb9
Fabiola2025-Mokuy <mokuyfabiola@gmail.com>
2025-09-03 15:32:39 +0200

    Up Date Assignment Week %

---
+++ script.js
@@ -0,0 +1,48 @@
+let car ={
+    make: "mazda",
+    model: "BT50",
+    year: 2018
+};
+console.log(car)
+
+let fruit = ['banana','citron','grape']
+console.log(fruit);
+
+let name = Fabiola;
+let gender = F;
+if (gender = M){
+    console.log(" you are Boy");
+    }
+    else{
+        console.log(" you are girl");
+    }
+
+
+function gender(Female)
+{if (Female)
+console.log ("good girl")
+}
+else{
+    console.log("good boy");
+}
+
+function maritalStatus (ms)
+{if (ms=s)
+    {console.log ("single");}
+else if (s=m){console.log("married");}
+else{ console.loge ("widower")}
+}
+
+
+for (let i=1;i<=10;i++);{
+    console.log("Well come-",i);
+}
+
+document.getElementsByClassName("name").addEventlistener("click", function(){document.getElementsByClassName("name").innerTex =" write name";});
+//begin manipulation
+body.style.background = "pink";
+classNamelassName.textContent = " Heading has been altered";
+className.style.color = "blue";
+ptext.style.fontSise ="20px";
+btn.style.boderColor ="marrow";
+btn.style.boder = "40px";
